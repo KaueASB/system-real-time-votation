c596927b47a3a660774f8682c7477946b90a0454
Kaue Alves <kaue_alves00@yahoo.com>
2024-02-06 11:38:49 -0300

    feat: create route create-poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,9 @@
 import fastify from 'fastify'
+import { createPoll } from './routes/create-poll'
 
 const app = fastify()
 
-app.get('/', () => {
-  return 'Hello NLW'
-})
+app.register(createPoll)
 
 app.listen({port: 3333}).then(() => {
   console.log('HTTP server running! 🚀')  
